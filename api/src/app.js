a7392b09de3e07a22bd2182f89f06e1ebb4bec1c
Flávio dos Santos <flavio.santos.ti@outlook.com>
2020-05-25 00:49:35 -0300

    corrigido

--- api/src/app.js
+++ api/src/app.js
@@ -7,7 +7,6 @@ app.use(express.json());
 app.use(express.json({ type: 'application/vnd.api+json' }));
 app.use(cors());
 
-
 const pg = require('pg');
 
 var conString = "postgres://postgres:mouse@localhost:5432/geomodelo";
@@ -17,29 +16,9 @@ var conString = "postgres://postgres:mouse@localhost:5432/geomodelo";
 const client = new pg.Client(conString);
 client.connect();
 
-//var http = require("http");
-//const pg = require('pg');
-
-//var conString = "postgres://postgres:mouse@localhost:5432/geomodelo";
-
-//var client = new pg.Client(conString);
-//client.connect();
-
-//var query = client.query("SELECT * FROM categoria");
-
-//client.query("SELECT * FROM categoria", (err, res) => {
-//    if (err) throw err
-//    console.log(res)
-//    client.end()
-//  });
-
-
 // Iniciando o express
 
 const port = 3000;
-
-//const router = express.Router();
-//console.log('passou');
 
 app.get('/api/', (req, res) => {
   res.status(200).send({
@@ -69,29 +48,13 @@ getMarca = async (req, res) => {
 
 updateMarca = async (req, res) => {
   
-  console.log('entrou no updateMarca');
-  console.log(req.body);
-  console.log('nm_marca  '+req.body.nm_marca);
-  console.log(" primeiro = $1");
-  
   const id_marca = parseInt(req.params.id);
   const { nm_marca, fg_ativo } = req.body;
-  console.log(" primeiro = $nm_marca");
-  
-  var sql = 'UPDATE marca SET '+ 
-  '  nm_marca = ${nm_marca},  '+
-  '  fg_ativo = ${fg_ativo} '+
-  'WHERE '+ 
-  '  id_marca = ${id_marca}';
-  //const response = await client.query(
-  await client.query(
-
-    sql
-    
-
-
-    );
-  await client.query('COMMIT');
+ 
+  const response = await client.query(
+    "UPDATE marca SET nm_marca = $1, fg_ativo = $2 WHERE id_marca = $3",
+    [nm_marca, fg_ativo, id_marca]
+  );
   
   res.status(200).send({ message: "Product Updated Successfully!" });
 };
